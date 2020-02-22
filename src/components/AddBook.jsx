994306de777dc76c3bd567656c047e8c958b5102
ajLapid718 <ajLapid718@gmail.com>
2020-02-22 01:50:22 -0500

    populate dropdown menu with authors

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -12,6 +12,16 @@ const getAuthorsQuery = gql`
 `;
 
 class AddBook extends Component {
+  displayAuthors = () => {
+    let data = this.props.data;
+    if (data.loading) {
+      return <option disabled>Loading Authors...</option>
+    }
+    else {
+      return data.authors.map(author => <option key={author.id} value={author.id}>{author.name}</option>)
+    }
+  }
+
   render() {
     return (
       <form id="add-book">
@@ -30,6 +40,7 @@ class AddBook extends Component {
           <label>Author</label>
           <select>
             <option>Select Author</option>
+            {this.displayAuthors()};
           </select>
         </div>
 
