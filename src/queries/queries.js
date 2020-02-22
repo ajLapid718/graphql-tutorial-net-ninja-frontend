e595b5f067020434f78397132589f07630f7f366
ajLapid718 <ajLapid718@gmail.com>
2020-02-22 02:37:43 -0500

    construct addBookMutation

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -1,7 +1,7 @@
 import { gql } from "apollo-boost";
 
 const getBooksQuery = gql`
-  {
+  query  {
     books {
       name
       id
@@ -10,7 +10,7 @@ const getBooksQuery = gql`
 `;
 
 const getAuthorsQuery = gql`
-  {
+  query {
     authors {
       name
       id
@@ -18,4 +18,13 @@ const getAuthorsQuery = gql`
   }
 `;
 
-export { getAuthorsQuery, getBooksQuery };
+const addBookMutation = gql`
+  mutation {
+    addBook(name: "", genre: "", authorId: "") {
+      name
+      id
+    }
+  }
+`
+
+export { getAuthorsQuery, getBooksQuery, addBookMutation };
