31d00b34f19540bd5da63caaf76102d3a2cd928a
ajLapid718 <ajLapid718@gmail.com>
2020-02-23 00:23:05 -0500

    grab id of clicked book and track it in local state object

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,13 +3,27 @@ import { graphql } from 'react-apollo';
 import { getBooksQuery } from '../queries/queries';
 
 class BookList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedBookId: null
+    }
+  }
+
+  handleClick = (bookId) => {
+    this.setState({ selectedBookId: bookId })
+  }
+
   displayBooks = () => {
     let data = this.props.data;
     if (data.loading) {
       return <div>Loading...</div>
     }
     else {
-      return data.books.map(book => <li key={book.id}>{book.name}</li>)
+      return data.books.map(book => {
+          return <li key={book.id} onClick={() => this.handleClick(book.id)}>{book.name}</li>
+        }
+      )
     }
   }
 
